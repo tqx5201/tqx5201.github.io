baf6ecb6e5ad3b89e996353498121de06d93699e
天仙婆婆 <tqx5201@163.com>
2024-01-27 22:06:31 +0800

    Update py_cntv.py

--- tvbox/fongmi/python/py_cntv.py
+++ tvbox/fongmi/python/py_cntv.py
@@ -213,11 +213,38 @@ class Spider(Spider):  # 元类 默认的元类 type
 		result = {}
 		return result
 
-	def playerContent(self, flag, pid, vipFlags):
+	def playerContent2(self, flag, pid, vipFlags):
 		result = {}
 		url = f"https://vdn.apps.cntv.cn/api/getHttpVideoInfo.do?pid={pid}"
 		data = self.fetch(url, headers=self.header, timeout=10).json()
 		url = data['hls_url'].strip()
+		result["parse"] = 0
+		result["playUrl"] = ''
+		result["url"] = url
+		result["header"] = ''
+		return result
+
+	def playerContent(self,flag,id,vipFlags):
+		result = {}
+		url = "https://vdn.apps.cntv.cn/api/getHttpVideoInfo.do?pid={0}".format(id)
+		jo = self.fetch(url,headers=self.header).json()
+		link = jo['hls_url'].strip()
+		rsp = self.fetch(link,headers=self.header)
+		content = rsp.text.strip()
+		arr = content.split('\n')
+		urlPrefix = self.regStr(link,'(http[s]?://[a-zA-z0-9.]+)/')
+
+		subUrl = arr[-1].split('/')
+		subUrl[3] = '1200'
+		subUrl[-1] = '1200.m3u8'
+		hdUrl = urlPrefix + '/'.join(subUrl)
+
+		url = urlPrefix + arr[-1]
+
+		hdRsp = self.fetch(hdUrl,headers=self.header)
+		if hdRsp.status_code == 200:
+			url = hdUrl
+
 		result["parse"] = 0
 		result["playUrl"] = ''
 		result["url"] = url
